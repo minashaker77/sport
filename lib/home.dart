b68d99c1f05dca50f353b0d3df967585387d4fbd
minashaker77 <Minashk1998@gmail.com>
2022-06-04 17:11:39 +0430

    create list view for week tab

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:skeletons/skeletons.dart';
 
 class MainPageController extends GetxController
     with GetSingleTickerProviderStateMixin {
@@ -44,6 +45,12 @@ class HomeScreen extends StatelessWidget {
       Icons.support_agent,
       CupertinoIcons.car,
       Icons.accessible_sharp
+    ];
+    List<String> listImage = [
+      "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
+      "https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg",
+      "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     ];
     Color? color = const Color(0xff4b4b87);
     return SafeArea(
@@ -188,9 +195,29 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [Center(child: Text("Week"))],
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                          itemCount: 4,
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (BuildContext context, index) {
+                            return Container(
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 15),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: DecorationImage(
+                                      image: NetworkImage(listImage[index]),
+                                      fit: BoxFit.cover)),
+                              height: 200,
+                              width: double.infinity,
+                            );
+                          })
+                    ],
+                  ),
                 ),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.center,
